154d0121fadb72759cd65ec5ae06d039f690c440
virbhanumohan <mohanrajam321@gmail.com>
2025-09-09 14:27:14 +0530

    updated README

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+
+class Apptheme{
+  static final darktheme = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+  );
+}
---
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Sign Up" , style: TextStyle(fontSize: 45.0,fontWeight: FontWeight.bold),),
+        ],
+      ),
+    );
+  }
+}
