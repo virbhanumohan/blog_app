7bb888e33496938b54db3f26ac270e87e7b79491
virbhanumohan <mohanrajam321@gmail.com>
2025-09-26 13:03:38 +0530

    Doing usecaseCore

---
+++ lib/core/usecase/usecase.dart
@@ -0,0 +1,6 @@
+// import 'package:fpdart/fpdart.dart';
+// import 'package:blog_app/core/error/failures.dart';
+
+// abstract interface class Usecase<> {
+//   Future<Either<Failure>>
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,10 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final supabase = await Supabase.initialize(url: AppSecrets.supabaseUrl,anonKey: AppSecrets.supabaseAnnonKey);
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnnonKey,
+  );
   runApp(const MyApp());
 }
 
